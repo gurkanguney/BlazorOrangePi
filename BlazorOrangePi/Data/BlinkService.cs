0259e709d3f2d92c9d791abeab2fb8a74bbb9699
Gürkan GÜNEY <gurkan_guney@hotmail.com>
2020-04-15 15:52:43 +0300

    15 Nisan Son durum

--- BlazorOrangePi/Data/BlinkService.cs
+++ BlazorOrangePi/Data/BlinkService.cs
@@ -3,39 +3,145 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Device.Gpio;
-
+using Iot.Device.DHTxx;
+using Iot.Device.OneWire;
+using Iot.Units;
+using System.Threading;
+using Iot.Device.Ssd13xx;
+using System.Device.I2c;
+using Iot.Device.Ssd13xx.Commands;
 
 namespace BlazorOrangePi.Data
 {
     public class BlinkService
     {
-        public static GpioController gpioController = new GpioController();
-        public static int ledPin = 68;
-        public bool ledDurum = false;
-        public BlinkService()
+        // Events
+        public delegate void PinDurumuDegistiDelegete(object sender, BlinkServiceChangedEventArgs args);
+        public delegate void SicaklikDegistiDelegate(object sender, SicaklikDegistiEventArgs args);
+
+        public event PinDurumuDegistiDelegete OnPinDurumuDegisti;
+        public event SicaklikDegistiDelegate OnSicaklikDegisti;
+
+
+
+        public static GpioController gpioController;
+        public static int ledPin = 110;  //PD14
+        private Timer zamanlayici;
+        public Exception SonHata;
+        private double _sicaklik;
+        private bool _ledDurum;
+
+
+        public double Sicaklik
         {
-            if (!gpioController.IsPinOpen(ledPin))
+            get { return _sicaklik; }
+            private set
             {
-                gpioController.OpenPin(ledPin, PinMode.Output);
-            }
-            else
-            {
-                ledDurum = (PinValue.High == gpioController.Read(ledPin));
+                if (_sicaklik != value)
+                {
+                    OnSicaklikDegisti(this, new SicaklikDegistiEventArgs(value, _sicaklik));
+                }
+                _sicaklik = value;
             }
         }
-        public void LedIslem(bool toggle)
+
+        public bool LedDurum
         {
-            if (toggle)
+            get { return _ledDurum; }
+            private set
+            {
+                if (_ledDurum != value)
+                {
+                    OnPinDurumuDegisti(this, new BlinkServiceChangedEventArgs(value, _ledDurum));
+                }
+                _ledDurum = value;
+            }
+
+        }
+
+        public void ServiceInit()
+        {
+            zamanlayici = new Timer(new TimerCallback(async _ =>
+            {
+                await DS18B20SicaklikOku();
+            }), null, 1000, 2000);
+        }
+        public BlinkService()
+        {
+            try
+            {
+                gpioController = new GpioController();
+
+                if (!gpioController.IsPinOpen(ledPin))
+                {
+                    gpioController.OpenPin(ledPin, PinMode.Output);
+                }
+
+
+            }
+            catch (Exception hata)
+            {
+                SonHata = hata;
+            }
+        }
+
+        public void LedIslem()
+        {
+            if (!LedDurum)
             {
                 gpioController.Write(ledPin, PinValue.High);
-                ledDurum = true;
+                LedDurum = true;
             }
             else
             {
                 gpioController.Write(ledPin, PinValue.Low);
-                ledDurum = false;
+                LedDurum = false;
+            }
+        }
+        public async Task DS18B20SicaklikOku()
+        {
+            try
+            {
+                string busId = OneWireBus.EnumerateBusIds().First();
+                string deviceId = OneWireDevice.EnumerateDeviceIds().First().devId;
+                OneWireThermometerDevice ds18B20Dev = new OneWireThermometerDevice(busId, deviceId);
+                Temperature sicaklik = await ds18B20Dev.ReadTemperatureAsync();
+
+                Sicaklik = sicaklik.Celsius;
+            }
+            catch (Exception hata)
+            {
+                SonHata = hata;
             }
 
         }
     }
+
+
+
+
+    public class BlinkServiceChangedEventArgs : EventArgs
+    {
+        public bool OncekiDurum { get; set; }
+        public bool Durum { get; set; }
+
+        public BlinkServiceChangedEventArgs(bool yeniDurum, bool oncekiDurum)
+        {
+            this.Durum = yeniDurum;
+            this.OncekiDurum = oncekiDurum;
+        }
+    }
+
+    public class SicaklikDegistiEventArgs : EventArgs
+    {
+        public double OncekiSicaklik { get; set; }
+        public double SonSicaklik { get; set; }
+
+        public SicaklikDegistiEventArgs(double yenisi, double eskisi)
+        {
+            this.OncekiSicaklik = eskisi;
+            this.SonSicaklik = yenisi;
+        }
+    }
+
 }
--- BlazorOrangePi/Startup.cs
+++ BlazorOrangePi/Startup.cs
@@ -10,6 +10,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BlazorOrangePi.Data;
+using BlazorOrangePi.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 namespace BlazorOrangePi
 {
@@ -26,10 +33,20 @@ namespace BlazorOrangePi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(optionsAction: options => {
+                options.UseSqlite(connectionString: "DataSource=app.db");
+            });
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<AppDbContext>();
             services.AddRazorPages();
             services.AddServerSideBlazor().AddCircuitOptions(options => { options.DetailedErrors = true; });
+            services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
             services.AddSingleton<WeatherForecastService>();
-            services.AddSingleton<BlinkService>();
+            // Sistem calisacak
+            BlinkService sistemServis = new BlinkService();
+            sistemServis.ServiceInit();
+            // Sistem basladiktan sonra 
+            services.AddSingleton<BlinkService>(sistemServis);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,11 +66,22 @@ namespace BlazorOrangePi
             }
 
             //app.UseHttpsRedirection();
+            RequestLocalizationOptions localizationOptions = new RequestLocalizationOptions
+            {
+                SupportedCultures = new List<CultureInfo> { new CultureInfo("en-US"), new CultureInfo("tr-TR") },
+                SupportedUICultures = new List<CultureInfo> { new CultureInfo("en-US"), new CultureInfo("tr-TR") },
+                DefaultRequestCulture = new RequestCulture("tr-TR")
+            };
+            app.UseRequestLocalization(localizationOptions);
             app.UseStaticFiles();
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
